2711ea6fc236a14bdf57b98733ec9c8c4091e48e
Vasily Postnicov <shamaz.mazum@gmail.com>
2026-03-24 09:55:10 +0300

    Compute initial approximation for nndescent

--- src/random-forest.lisp
+++ src/random-forest.lisp
@@ -3,7 +3,9 @@
   (:local-nicknames (#:q  #:nndescent/pqueue)
                     (#:p  #:nndescent/point)
                     (#:rt #:nndescent/random-tree))
-  (:export #:make-random-forest))
+  (:export #:make-random-forest
+           #:initial-approximation
+           #:dequeue-approximation))
 (in-package :nndescent/random-forest)
 
 (serapeum:-> make-random-forest (p:operations list (integer 1) (integer 1))
@@ -12,3 +14,41 @@
   (mapcar #'lparallel:force
           (loop repeat n
                 collect (lparallel:future (rt:make-random-tree ops ps k)))))
+
+(serapeum:-> initial-approximation (p:operations list (integer 1))
+             (values (simple-array q:queue (*)) &optional))
+(defun initial-approximation (ops ps k)
+  (declare (optimize (speed 3)))
+  (let ((forest (make-random-forest ops ps k 5)))
+    (flet ((make-queue (p)
+             (let ((q (q:make-queue k)))
+               (loop for tree in forest
+                     for neighbors = (rt:neighbor-points ops tree p) do
+                       (loop for %p in neighbors
+                             for dist = (p:dist ops p %p)
+                             unless (or (eq p %p)
+                                        (q:in-queue-p %p q :test #'eq))
+                               do (q:enqueue-limited q %p (- dist) k)))
+               q)))
+      ;; I don't know why, this thing gives wrong results when parallelized
+      #+nil
+      (let ((qs (loop for p in ps collect (lparallel:future (make-queue p)))))
+        (make-array (length ps)
+                    :element-type 'q:queue
+                    :initial-contents (mapcar #'lparallel:force qs)))
+      (make-array (length ps)
+                  :element-type 'q:queue
+                  :initial-contents (loop for p in ps collect (make-queue p))))))
+
+(serapeum:-> queue->list (q:queue)
+             (values list &optional))
+(defun queue->list (q)
+  (let ((q (q:copy-queue q)) acc)
+    (loop for obj = (q:dequeue q)
+          while obj do (push obj acc))
+    acc))
+
+(serapeum:-> dequeue-approximation ((simple-array q:queue (*)))
+             (values simple-vector &optional))
+(defun dequeue-approximation (approx)
+  (map 'vector #'queue->list approx))
